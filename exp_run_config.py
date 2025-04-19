7a596d5e64387e3a01887f2ec5d5c10455388926
Lotzi Bölöni <lotzi.boloni@gmail.com>
2025-04-19 12:15:56 -0400

    Continue exp/run/subrun refactoring

--- exp_run_config.py
+++ exp_run_config.py
@@ -19,10 +19,10 @@ Config()["group"]["value"]
 """
 
 import yaml
-from pathlib import Path
+import pathlib
+from datetime import datetime
 
 # PROJECTNAME = "WaterBerryFarms"
-
 
 class Experiment:
     """A class encapsulating an experiment"""
@@ -31,37 +31,78 @@ class Experiment:
 
     def __getitem__(self, key):
         return self.values[key]
-    
+
+    def __setitem__(self, key, value):
+        self.values[key] = value
+        self.save()
+
     def __repr__(self):
         """FIXME: print the experiment out in a readable way"""
-        return "Experiment: {self.values}"
+        return f"Experiment: {self.values}"
     
+    def data_dir(self):
+        return pathlib.Path(self.values[Config.DATA_DIR])
+
+    def experiment(self):
+        return self.values[Config.EXPERIMENT_NAME]
+
+    def run(self):
+        return self.values[Config.RUN_NAME]
+
+    def subrun(self):
+        return self.values[Config.SUBRUN_NAME]
+
     def clean(self):
         """Programatically clean up an experiment to allow new run"""
         raise Exception("Experiment.clean not implemented yet")
+
+    def save(self):
+        with open(pathlib.Path(self.data_dir(), "exprun.yaml"), "w") as f:
+            yaml.dump(self.values, f)
+
+    def set_time_started(self):
+        now = datetime.now()
+        self.values[Config.TIME_STARTED] = now.strftime(Config.TIME_FORMAT)
+
+    def get_time_started(self):
+        parsed = datetime.strptime(self.values[Config.TIME_STARTED], Config.TIME_FORMAT)
+        return parsed
+    
+    def done(self):
+        """Sets the time done variable, and saves"""
+        now = datetime.now()
+        self.values[Config.TIME_DONE] = now.strftime(Config.TIME_FORMAT)
+        self.save()
 
 class Config:
     """The overall settings class. """
     _instance = None  # Class-level attribute to store the instance
 
-    PREFIX = "** ExpRun ** : "
+    PREFIX = "***ExpRun**: "
     PROJECTNAME = None
+    DATA_DIR = "data_dir"
+    EXPERIMENT_NAME = "experiment_name"
+    RUN_NAME = "run_name"
+    SUBRUN_NAME = "subrun_name"
+    TIME_STARTED = "time_started"
+    TIME_DONE = "time_done"
+    TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
     def __new__(cls, *args, **kwargs):
         if Config.PROJECTNAME is None:
             Config.__log(f"Config.PROJECTNAME not set, assign it before using the exp/run framework")
         if not cls._instance:
             cls._instance = super(Config, cls).__new__(cls)
-            home_directory = Path.home()
-            main_config = Path(home_directory, ".config", Config.PROJECTNAME, "mainsettings.yaml")
-            Config.__log(f"Loading pointer config file:\n\t {main_config}")
+            home_directory = pathlib.Path.home()
+            main_config = pathlib.Path(home_directory, ".config", Config.PROJECTNAME, "mainsettings.yaml")
+            Config.__log(f"Loading pointer config file:\n\t{main_config}")
             if not main_config.exists():
                 raise Exception(f"Missing pointer config file: {main_config}")
             with main_config.open("rt") as handle:
                 main_config = yaml.safe_load(handle)
             configpath = main_config["configpath"]
-            Config.__log(f"Loading machine-specific config file: {configpath}")
-            configpath = Path(configpath)
+            Config.__log(f"Loading machine-specific config file:\n\t{configpath}")
+            configpath = pathlib.Path(configpath)
             if not configpath.exists():
                 raise Exception(f"Missing machine-specific config file:\n\t {configpath}")
             with configpath.open("rt") as handle:
@@ -79,64 +120,73 @@ class Config:
         print(Config.PREFIX, end="")
         print(pattern, flush=True)
 
-    def get_experiment(self, group_name, run_name, subrun_name=None):
+    def get_experiment(self, experiment_name, run_name, subrun_name=None):
         """Returns an experiment configuration, which is the 
         mixture between the system-dependent configuration and the system independent configuration."""
-        current_directory = Path(__file__).resolve().parent
+        current_directory = pathlib.Path(__file__).resolve().parent
         #
         # Load the system independent group configuration
         #
-        experiment_group_indep = Path(current_directory, "experiment_configs", group_name, "_" + group_name + ".yaml")
+        experiment_group_indep = pathlib.Path(current_directory, "experiment_configs", experiment_name, "_" + experiment_name + ".yaml")
         if not experiment_group_indep.exists():
             raise Exception(f"Missing experiment default config {experiment_group_indep}")
         with experiment_group_indep.open("rt") as handle:
             group_config = yaml.safe_load(handle)
         if group_config == None:
-            print(f"Experiment default config {experiment_group_indep} was empty, ok.")
+            self.__log(f"Experiment default config {experiment_group_indep} was empty, ok.")
             group_config = {}
-        group_config["group_name"] = group_name
+        group_config[Config.EXPERIMENT_NAME] = experiment_name
         #
         # Load the system independent run configuration
         #
-        experiment_run_indep = Path(current_directory, "experiment_configs", group_name, run_name + ".yaml")
+        experiment_run_indep = pathlib.Path(current_directory, "experiment_configs", experiment_name, run_name + ".yaml")
         if not experiment_run_indep.exists():
             raise Exception(f"Missing experiment system independent config file {experiment_run_indep}")
         with experiment_run_indep.open("rt") as handle:
             indep_config = yaml.safe_load(handle)
         if indep_config == None:
-            print("The system independent config file of the experiment {experiment_run_indep} was empty, this is a likely error, but continuing.")
+            self.__log(f"The system independent config file of the experiment\n\t{experiment_run_indep}\n\t was empty, this is a likely error, but continuing.")
             indep_config = {}
 
-        indep_config["run_name"] = run_name
+        indep_config[Config.RUN_NAME] = run_name
         indep_config = group_config | indep_config
         # create the data dir
         if "experiment_data" not in self.values:            
-            print(f"The key 'experiment_data' must be present in the configuration. \nThis is the parent directory from where the experiment data should go.")
-            print(f"If all the data for all the experiments go to the same directory, \nan appropriate place for this is the system dependent config file, \n in this case: {self['configpath']}")
+            self.__log(f"The key 'experiment_data' must be present in the configuration.\n\tThis is the parent directory from where the experiment data should go.")
+            self.__log(f"If all the data for all the experiments go to the same directory, \n\tan appropriate place for this is the system dependent config file, in this case:\n\t {self['configpath']}")
             raise Exception("Missing experiment_data specification")
-        data_dir = Path(self.values["experiment_data"], group_name, run_name)
-        data_dir.mkdir(exist_ok=True, parents=True)
-        indep_config["data_dir"] = data_dir
-        indep_config["exp_run_sys_indep_file"] = experiment_run_indep
+        indep_config["exp_run_sys_indep_file"] = str(experiment_run_indep)
         #
         # Load the system dependent run configuration
         #
         if "experiment_system_dependent_dir" not in self.values:            
-            print(f"The key 'experiment_system_dependent_dir' must be present in the configuration. \nThis is the parent directory from where system dependent part of the experiment configurations must go.")
-            print(f"An appropriate place to specify this is the system dependent config file, \n in this case: {self['configpath']}")
+            self.__log(f"The key 'experiment_system_dependent_dir' must be present in the configuration.\n\tThis is the parent directory from where system dependent part of the experiment configurations must go.")
+            self.__log(f"An appropriate place to specify this is the system dependent config file, in this case:\n\t{self['configpath']}")
             raise Exception("Missing experiment_system_dependent_dir specification")
 
-        experiment_directory = Path(self.values["experiment_system_dependent_dir"])
-        experiment_sys_dep = Path(experiment_directory, group_name, run_name + "_sysdep.yaml")
+        experiment_directory = pathlib.Path(self.values["experiment_system_dependent_dir"])
+        experiment_sys_dep = pathlib.Path(experiment_directory, experiment_name, run_name + "_sysdep.yaml")
         if not experiment_sys_dep.exists():
-            print(f"No system dependent experiment file\n {experiment_sys_dep},\n that is ok, proceeding.")
+            self.__log(f"No system dependent experiment file\n\t {experiment_sys_dep},\n\t that is ok, proceeding.")
             exp_config = indep_config
         else: 
             with experiment_sys_dep.open("rt") as handle:
                 dep_config = yaml.safe_load(handle)
             exp_config = indep_config | dep_config
-            exp_config["exp_run_sys_dep_file"] = experiment_sys_dep
+            exp_config["exp_run_sys_dep_file"] = str(experiment_sys_dep)
 
-        print(f"Configuration for experiment: {group_name}/{run_name} successfully loaded", flush=True)
+        self.__log(f"Configuration for exp/run: {experiment_name}/{run_name} successfully loaded")
 
-        return Experiment(exp_config)
+        # creating the data dir - FIXME this should be subdir based
+        if subrun_name is None:
+            data_dir = pathlib.Path(self.values["experiment_data"], experiment_name, run_name)
+        else:
+            data_dir = pathlib.Path(self.values["experiment_data"], experiment_name, run_name, subrun_name)
+        exp_config[Config.DATA_DIR] = str(data_dir)
+        exp_config[Config.SUBRUN_NAME] = subrun_name
+        data_dir.mkdir(exist_ok=True, parents=True)
+
+        exp = Experiment(exp_config)
+        exp.set_time_started()
+        exp.save()
+        return exp
