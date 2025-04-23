83be13721f3d54c61485d1b7e41f906764648be2
Lotzi Bölöni <lotzi.boloni@gmail.com>
2025-04-23 17:27:28 -0400

    Created the outlines of the majority of functions

--- algorithms/mrmd/mrmr_policies.py
+++ algorithms/mrmd/mrmr_policies.py
@@ -139,16 +139,73 @@ class MRMR_Contractor(AbstractCommunicateAndFollowPath):
         print("MRMR_Contractor policy was created")
         # the current epoffer which is under execution
         self.current_epoffer = None
+        self.plan = self.replan()
     
-    def plan_current_path():
+    def create_ep_xyplan(self, eps, time):
+        """Create the xyplan for the eps. Mark each segment with the specific ep, and the intermediate ones with no ep."""
+        # FIXME: implement me
+        return []
+
+    def create_random_waypoint_xyplan(self, time):
+        """Create a random waypoint xy plan starting from time to the time 
+        described in the budget of the policy"""
+        # FIXME: implement me
+        return []
+
+    def replan(self):
         """Plan a path that covers the eps accepted but not terminated"""
         # FIXME implement me
+        # Path:
+        # [{"x":4, "y":4, "ep":None,}],....
+        oldplan = self.plan
+        self.plan = []
+        # part one: copy the remainder of the current ep
+        if len(oldplan) > 0 and oldplan[0]["ep"] is not None:
+            currentep = oldplan[0]["ep"]
+            while True:
+                step = oldplan.pop()
+                if step["ep"] != currentep:
+                    break
+                self.plan.append(step)
+        # part two: create a plan accross the eps remaining
+        t = self.plan[-1]["t"]
+        eps = self.epagent.commitments
+        ep_plan = self.create_ep_plan(eps, t+1)
+        self.plan.append(ep_plan)
+        # part three: create random waypoint to the rest
         return None
-
+    
+    def can_bid(self, epoffer):
+        """This function allows the agent to decide whether it can bid for a certain offer or not."""
+        # step one: find the termination time of the current ep
+        t = self.robot.env.t
+        if self.plan[0]["ep"] is not None: # we are in an ep
+            i = 0
+            currentep = self.plan[0]["ep"]
+            while True:
+                t = self.plan[i]["t"]
+                i+=1
+                if self.plan[i]["ep"] != currentep:
+                    break
+        t = t + 1
+        # get all the eps, except the current one
+        eps = self.epagent.commitments
+        eps.remove(currentep)
+        eps.add(epoffer.ep)
+        # create an optimal path 
+        path = self.create_ep_xyplan(eps, t)
+        if path[-1]["t"] < self.exp["budget"]:
+            return True # we can bid
+        return False # we cannot bid
 
     def act(self, delta_t):
         """Call the following of the path"""
-        super().act(delta_t)
+        # super().act(delta_t)
+        # just verify that the time is the right one
+        assert self.plan[0]["t"] != self.robot.env.t
+        self.robot.add_action(f"loc [{self.plan[0]['x']}, {self.plan[0]['y']}]")
+        # set the current location
+
 
     def act_send(self, round):
         """FIXME: the market negotiation was supposed to happen here
