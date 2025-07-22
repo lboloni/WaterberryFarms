c629376d92bd721d4567b80016eca7390165a7cf
Lotzi Bölöni <lotzi.boloni@gmail.com>
2025-07-22 16:34:09 -0400

    Automation added to WBF

--- automate.py
+++ automate.py
@@ -29,22 +29,23 @@ def automate_exprun(notebook, name, params):
    except Exception as e:
       print(f"There was an exception {e}")
 
-if len(sys.argv) < 2:
-   print("No argument passed, running automate_00")
-   experiment = "automate"
-   run = "automate_short"
-   exp = Config().get_experiment(experiment, run)
-else:
-   print(f"Running script {sys.argv[1]}")
-   yaml_path = pathlib.Path(sys.argv[1])
-   if not yaml_path.is_file():
-      print(f"Error: File '{yaml_path}' does not exist.")
-      sys.exit(1)
-   # Load the YAML file into a dictionary
-   with open(yaml_path, 'r') as f:
-      exp = yaml.safe_load(f)      
+if __name__ == "__main__":
+   if len(sys.argv) < 2:
+      print("No argument passed, running automate_00")
+      experiment = "automate"
+      run = "automate_short"
+      exp = Config().get_experiment(experiment, run)
+   else:
+      print(f"Running script {sys.argv[1]}")
+      yaml_path = pathlib.Path(sys.argv[1])
+      if not yaml_path.is_file():
+         print(f"Error: File '{yaml_path}' does not exist.")
+         sys.exit(1)
+      # Load the YAML file into a dictionary
+      with open(yaml_path, 'r') as f:
+         exp = yaml.safe_load(f)      
 
-for item in tqdm(exp["exps_to_run"]):
-    print(f"***Automating {item['name']}")
-    #notebook = params["notebook"]
-    automate_exprun(item["notebook"], item["name"], item["params"])
+   for item in tqdm(exp["exps_to_run"]):
+      print(f"***Automating {item['name']}")
+      #notebook = params["notebook"]
+      automate_exprun(item["notebook"], item["name"], item["params"])
